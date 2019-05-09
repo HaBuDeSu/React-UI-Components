1120952a124deb6eb366ff982ad56c3cab8c6734
HaBuDeSu <ruf.kenichi@gmail.com>
2019-05-08 23:10:15 -0500

    got the footer to show but the images aren't loading

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const FooterContainer = () => {
+  return (
+    <div className="footer-container">
+      <img src=''></img>
+      <img src="../FooterComponents/retweet-icon.png"></img>
+      <img src="./heart-icon.png"></img>
+      <img src="./email-icon.png"></img>
+    </div>
+  )
+};
+
+export default FooterContainer;
