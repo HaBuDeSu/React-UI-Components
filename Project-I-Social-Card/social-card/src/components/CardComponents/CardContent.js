b5ba2d5e617f19b0ca0f39faa38af5e31df68196
HaBuDeSu <ruf.kenichi@gmail.com>
2019-05-08 20:59:53 -0500

    got all the files hooked up

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div>
+      <p className="card-content-title">Get started with React</p>
+      <p className="card-content-content">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+      <p className="card-content-source">reactjs.org</p>
+    </div>
+  )
+};
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <span>LambdaSchool</span>
+  )
+};
+
+export default HeaderTitle;
